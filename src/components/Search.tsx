ee180c1ecf777ee8a529d2772a88b9017b31ba04
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-11-29 13:08:23 +0100

    refactor: add 2-way binding

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,6 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import SearchIcon from '../assets/icons/search.svg?react'
 
 interface Props {
     queryParam: string
@@ -8,6 +9,13 @@ interface Props {
 const Search: React.FC<Props> = ({ queryParam }) => {
     const [searchTerm, setSearchTerm] = useState<string>('')
     const [searchParams, setSearchParams] = useSearchParams()
+
+    useEffect(() => {
+        const initialSearchTerm = searchParams.get(queryParam)
+        if (initialSearchTerm) {
+            setSearchTerm(initialSearchTerm)
+        }
+    }, [queryParam, searchParams])
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value
@@ -22,13 +30,16 @@ const Search: React.FC<Props> = ({ queryParam }) => {
     }
 
     return (
-        <input
-            type="text"
-            value={searchTerm}
-            onChange={handleInputChange}
-            placeholder="Search..."
-            className="p-2 border rounded"
-        />
+        <div className="flex content-start items-center gap-3 bg-neutral-500-0.1 py-2 px-3 rounded-xl lg:py-3 lg:px-4">
+            <SearchIcon className="size-6 flex-none" />
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
+                placeholder="Search"
+                className="bg-transparent text-label-large text-primary-950 outline-0"
+            />
+        </div>
     )
 }
 
