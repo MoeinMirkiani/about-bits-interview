dbd1a8b71f67c4d4f481a614e06e30714822a32f
Moein Mirkiani <moein.mirkiani@gmail.com>
2024-11-30 17:35:18 +0100

    refactor: update pagination style

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
+import Button from "./Button.tsx"
+import ChevronRightIcon from '../assets/icons/chevron-right.svg?react'
+import ChevronLeftIcon from '../assets/icons/chevron-left.svg?react'
 
 interface PaginationProps {
     totalPages: number
@@ -24,74 +27,69 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage }) => {
 
         // Always show the first page
         pages.push(
-            <button
+            <Button
                 key={1}
                 onClick={() => updatePage(1)}
-                className={`px-2 py-1 ${currentPage === 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
             >
                 1
-            </button>
+            </Button>
         )
 
         // Add ellipses if needed after the first page
         if (currentPage > 4) {
-            pages.push(<span key="start-ellipsis">...</span>)
+            pages.push(<span key="text-label-medium start-ellipsis">..</span>)
         }
 
         // Show two pages before the current page
         for (let i = Math.max(2, currentPage - 2); i < currentPage; i++) {
             pages.push(
-                <button
+                <Button
                     key={i}
                     onClick={() => updatePage(i)}
-                    className={`px-2 py-1 ${currentPage === i ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
                 >
                     {i}
-                </button>
+                </Button>
             )
         }
 
         // Show the current page
         if (currentPage !== 1 && currentPage !== totalPages) {
             pages.push(
-                <button
+                <Button
                     key={currentPage}
                     onClick={() => updatePage(currentPage)}
-                    className={`px-2 py-1 bg-blue-500 text-white`}
                 >
                     {currentPage}
-                </button>
+                </Button>
             )
         }
 
         // Show two pages after the current page
         for (let i = currentPage + 1; i <= Math.min(totalPages - 1, currentPage + 2); i++) {
             pages.push(
-                <button
+                <Button
                     key={i}
                     onClick={() => updatePage(i)}
-                    className={`px-2 py-1 ${currentPage === i ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
                 >
                     {i}
-                </button>
+                </Button>
             )
         }
 
         // Add ellipses if needed before the last page
         if (currentPage < totalPages - 3) {
-            pages.push(<span key="end-ellipsis">...</span>)
+            pages.push(<span key="end-ellipsis">..</span>)
         }
 
         // Always show the last page
         if (totalPages > 1) {
             pages.push(
-                <button
+                <Button
                     key={totalPages}
                     onClick={() => updatePage(totalPages)}
-                    className={`px-2 py-1 ${currentPage === totalPages ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
                 >
                     {totalPages}
-                </button>
+                </Button>
             )
         }
 
@@ -99,26 +97,26 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage }) => {
     }
 
     return (
-        <div className="flex justify-between">
-            <button
+        <div className="flex justify-between px-2 py-4 bg-neutral-50 border-t border-t-neutral-200 md:px-4">
+            <Button
                 onClick={() => updatePage(currentPage - 1)}
                 disabled={currentPage <= 1}
-                className="px-2 py-1 bg-gray-200 disabled:opacity-50"
+                prependIcon={<ChevronLeftIcon />}
             >
-                Previous
-            </button>
+                <span className="hidden md:inline-flex">Previous</span>
+            </Button>
 
-            <div className="flex justify-center items-center gap-2">
+            <div className="flex justify-center items-center">
                 {renderPageNumbers()}
             </div>
 
-            <button
+            <Button
                 onClick={() => updatePage(currentPage + 1)}
                 disabled={currentPage >= totalPages}
-                className="px-2 py-1 bg-gray-200 disabled:opacity-50"
+                appendIcon={<ChevronRightIcon/>}
             >
-                Next
-            </button>
+                <span className="hidden md:inline-flex">Next</span>
+            </Button>
         </div>
     )
 }
